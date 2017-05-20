efcf88773df9395898844faf655b1c235b7939d2
intaglioplate312 <helenelizabeth@me.com>
2017-05-20 06:48:43 -0500

    working game functions, responsive design adequate, needs finessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,74 @@
+//Starting variables
 //Letter choices available
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//Setting all to zero
+//Variables set to zero
 var wins = 0;
 var losses = 0;
-var guesses = 13;
-var guessesLeft = 13;
+var guesses = 9;
+var guessesLeft = 9;
 var guessedLetters = [];
 
+//Computer selects letter
+var letterToGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+/* for initial test document.getElementById('computer').innerHTML = "Computer Choice: " + letterToGuess;*/
 
-
-
-//Computer selects letter found don't understand floor... ask Brett or Nathan
-var computerChoices = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-//Allows the user 9 guesses
+//Reset varibles
+//Grabbing the HTML element and setting it equal to the guessesLeft
 var updateGuessesLeft = function() {
-  // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
-  document.getElementById('guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+    document.getElementById('guessLeft').innerHTML = "Guesses left: " + guessesLeft;
 };
 
+//Grabbing the HTML element and setting it equal to the guessesLeft
 var updateLetterToGuess = function() {
-  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+    this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
 };
+
+// Display it as letters separated by commas, still not sure why join works and + doesn't
 var updateGuessesSoFar = function() {
-  // Display it as letters separated by commas. 
-  document.getElementById('letter').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+    document.getElementById('letter').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
 };
 
  // Function will be called when we reset everything
 var reset = function() {
-  totalGuesses = 13;
-  guessesLeft = 13;
-  guessedLetters = [];
-
-  updateLetterToGuess();
-  updateGuessesLeft();
-  updateGuessesSoFar();
+    totalGuesses = 9;
+    guessesLeft = 9;
+    guessedLetters = [];
+  
+    updateLetterToGuess();
+    updateGuessesLeft();
+    updateGuessesSoFar();
 }
 
-
-//When key is released it becomes the users guess
+//Basic game funciton
+//Key is press, the users guess -- need lowercase as computer doesn't pick Caps-- ask why
 document.onkeyup = function(event) {
     guessesLeft--;
-  var userGuess = String.fromCharCode(event.keyCode);
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();;
 
-  guessedLetters.push(userGuess);
-  updateGuessesLeft();
-  updateGuessesSoFar();
+    guessedLetters.push(userGuess);
+    updateGuessesLeft();
+    updateGuessesSoFar();
 
-        if (guessesLeft > 0){
-            if (userGuess === computerChoices){
-                wins++;
-                document.getElementById('wins').innerHTML = "Wins: " + wins;
-            }
-        
-        }else if(guessesLeft === 0){
-            // Then we will loss and we'll update the html to display the loss 
+      if (guessesLeft > 0){
+          // Update the html to display win
+      if (userGuess == letterToGuess){
+          wins++;
+          document.querySelector('#wins').innerHTML = "Wins: " + wins;
+          // Call the reset. 
+          reset();
+      }
+      }
+      else if(guessesLeft == 0){
+            // Update the html to display the loss 
             losses++;
-            document.getElementById('losses').innerHTML = "Losses: " + losses;
-           
-            // Then we'll call the reset. 
+            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+            // Call the reset. 
             reset();
-        }
+      }
+      };
 
-};
+
+
+
+
